527dfc28c9222d206ca7ba8c07782f96360520b4
jrajalu <jrajalu@gmail.com>
2013-08-22 16:29:42 +0800

    Update

--- header.php
+++ header.php
@@ -6,16 +6,14 @@
  */
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
+<html <?php language_attributes(); ?> class="no-js">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title><?php bloginfo('name'); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -29,12 +27,6 @@
 	<a href="<?php bloginfo('url'); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
 </div>
 <div class="container secondary-navigation">
-		<?php wp_nav_menu(array(
-			'theme_location' => 'secondary',
-			'menu' => 'Secondary Menu',
-			'container_id' => 'cssmenu',
-			'walker' => new CSS_Menu_Maker_Walker()
-			)); 
-		?>
+	<?php get_template_part( 'inc/secondary', 'menu' ); ?>
 </div>
 </header>
--- single.php
+++ single.php
@@ -5,8 +5,7 @@
  * @since Twenty Twelve 1.0
  */
 get_header(); ?>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>	
 <article class="container">
 	<h2><?php the_title(); ?></h2>
 	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
