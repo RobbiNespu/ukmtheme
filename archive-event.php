91c4015c37be8196529049ab32a3d88ff8c12a9a
Jamaludin Rajalu <jrajalu@gmail.com>
2014-01-31 00:30:05 +0800

    update to 6.1.9

--- archive-event.php
+++ archive-event.php
@@ -26,6 +26,7 @@ get_header(); ?>
       <div class="col-3-4">
         <h3 class="content-title"><?php the_title(); ?></h3>
         <p><?php echo get_post_meta($post->ID, 'ut_event_summary', true); ?></p>
+        <a href="<?php echo get_permalink(); ?>"><button class="uk-button uk-button-mini uk-button-primary"><?php _e('Read More','ukmtheme'); ?></button></a>
       </div>
     </div>   
     <?php endwhile; else: ?>
