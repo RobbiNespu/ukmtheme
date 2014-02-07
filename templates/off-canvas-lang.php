01755e32863414e10a7ae3e46f2e87e7ec14e20f
jrajalu <jrajalu@gmail.com>
2014-02-07 14:52:35 +0800

    no message

--- templates/off-canvas-lang.php
+++ templates/off-canvas-lang.php
@@ -4,9 +4,9 @@
   <p><?php _e( 'Press esc key to close', 'ukmtheme' ); ?></p>
     <h4><?php _e( 'Theme', 'ukmtheme' ); ?></h4>
     <ul>
-      <li><a href="#" class="reset_btn"><i class="uk-icon-circle tools-color-1st">&nbsp;</i><?php _e('Default','ukmtheme'); ?></a></li>
-      <li><a href="#" class="snd_btn"><i class="uk-icon-circle tools-color-2nd">&nbsp;</i><?php _e('Option #1','ukmtheme'); ?></a></li>
-      <li><a href="#" class="trd_btn"><i class="uk-icon-circle tools-color-3rd">&nbsp;</i><?php _e('Option #2','ukmtheme'); ?></a></li>
+      <li><a href="#" class="reset_btn"><i class="uk-icon-circle tools-color-1st">&nbsp;</i><?php _e('Theme One','ukmtheme'); ?></a></li>
+      <li><a href="#" class="snd_btn"><i class="uk-icon-circle tools-color-2nd">&nbsp;</i><?php _e('Theme Two','ukmtheme'); ?></a></li>
+      <li><a href="#" class="trd_btn"><i class="uk-icon-circle tools-color-3rd">&nbsp;</i><?php _e('Theme Three','ukmtheme'); ?></a></li>
     </ul>
     <h4><?php _e( 'Font Size', 'ukmtheme' ); ?></h4>
     <ul>
