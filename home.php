3725e472de23e33c237e03657bf8d589f2797200
jrajalu <jrajalu@gmail.com>
2014-06-04 09:45:04 +0800

    no message

--- home.php
+++ home.php
@@ -20,13 +20,15 @@ get_header(); ?>
     $widget_basic           = get_option('ukmtheme_widget_basic');
     $widget_custom          = get_option('ukmtheme_widget_custom');
     $widget_facebook        = get_option('ukmtheme_widget_facebook');
-    
-    get_template_part( 'templates/widget', $widget_news_scroller );
-    get_template_part( 'templates/widget', $widget_basic );
-    get_template_part( 'templates/widget', $widget_custom );
-    get_template_part( 'templates/widget', $widget_three_box );
-    get_template_part( 'templates/widget', $widget_four_box );
-    get_template_part( 'templates/widget', $widget_facebook );
+   
+   // SUSUNAN WIDGET MUKA HADAPAN
+
+    /** 1. */ get_template_part( 'templates/widget', $widget_news_scroller );
+    /** 2. */ get_template_part( 'templates/widget', $widget_basic );
+    /** 3. */ get_template_part( 'templates/widget', $widget_custom );
+    /** 4. */ get_template_part( 'templates/widget', $widget_three_box );
+    /** 5. */ get_template_part( 'templates/widget', $widget_four_box );
+    /** 6. */ get_template_part( 'templates/widget', $widget_facebook );
   ?>
   </div>
 </div>
