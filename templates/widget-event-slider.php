5b5b59b280245df240b5f4b1584907bf4a58054f
jrajalu <jrajalu@gmail.com>
2014-07-23 12:24:12 +0800

    no message

--- templates/widget-event-slider.php
+++ templates/widget-event-slider.php
@@ -8,11 +8,20 @@
  *
  * @author Jamaludin Rajalu
  */
-  $args = array( 'post_type' => 'event', 'posts_per_page' => 4 );
-  $eveSlider = new WP_Query( $args );
+  $current_year = date('Y');
+  $current_month = date('m');
+  $eveData = array(
+    'post_type'       => 'event',
+    'posts_per_page'  => 6,
+    'year'            => $current_year,
+    'monthnum'        => $current_month,
+    'post_status'     => array( 'publish', 'future' ),
+    );
+    
+  $eveSlider = new WP_Query( $eveData );
 ?>
-<script>
-  $(function() {
+<script type="text/javascript">
+$(function() {
 
   $('#carousel').carouFredSel({
     width: 897,
@@ -32,18 +41,16 @@
 </script>
 
 <div class="wrap">
-
-<div id="event_slider">
-  <div id="carousel">
-  <?php while ( $eveSlider->have_posts() ) : $eveSlider->the_post(); ?>
-    <div>
-      <h3 class="new_event_heading"><?php echo get_post_meta($post->ID, 'ut_event_date', true); ?></h3>
-      <span class="new_event_content"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></span>
+  <div id="event_slider">
+    <div id="carousel">
+    <?php while ( $eveSlider->have_posts() ) : $eveSlider->the_post(); ?>
+      <div>
+        <h3 class="new_event_heading"><?php echo get_post_meta($post->ID, 'ut_event_date', true); ?></h3>
+        <span class="new_event_content"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></span>
+      </div>
+      <?php endwhile; ?>
     </div>
-    <?php endwhile; ?>
+    <a id="prev" href="#"></a>
+    <a id="next" href="#"></a>
   </div>
-  <a id="prev" href="#"></a>
-  <a id="next" href="#"></a>
-</div>
-
 </div>
