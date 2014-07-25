248543e7ec3d9c8a68f80bb77a30614679f9ad70
jrajalu <jrajalu@gmail.com>
2014-07-25 11:22:20 +0800

    no message

--- footer.php
+++ footer.php
@@ -14,13 +14,13 @@ wp_footer(); ?>
 
 <div class="wrap pure-g pure-g-r">
   <div class="pure-u-1-2 footer-left-content">   
-    <p><?php _e( 'Best view: Viewable with any modern web browser (Desktop &amp; Mobile)', 'ukmtheme' ); ?></p>
     <?php if (dynamic_sidebar( 'sidebar-6' )) : else : endif; ?>
   </div>
   <div class="pure-u-1-2 footer-right-content">
     <?php get_template_part( 'templates/footer', 'social-link' ); ?>
     <p class="visitor-counter"><?php get_template_part( 'templates/visitor', 'counter' ); ?></p>
     <p><?php _e( 'Copyright &copy;', 'ukmtheme' ); ?><?php echo date( 'Y' ); ?><?php _e( 'The National University of Malaysia', 'ukmtheme' ); ?></p>
+    <p><?php _e( 'Best view: Viewable with any modern web browser (Desktop &amp; Mobile)', 'ukmtheme' ); ?></p>
     <p><?php _e('Powered by','ukmtheme'); ?>&nbsp;<a href="http://www.ukm.my/template/" target="_blank">UKMTheme</a></p>
   </div>
 </div>
