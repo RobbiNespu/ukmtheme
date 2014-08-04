8c0352e1b0dddaceeb08c2f2d553fcacd3c6783d
Jamaludin Rajalu <jrajalu@gmail.com>
2014-08-04 20:43:25 +0800

    no message

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@ wp_footer(); ?>
   <div class="pure-u-1-2 footer-right-content">
     <?php get_template_part( 'templates/footer', 'social-link' ); ?>
     <p class="visitor-counter"><?php get_template_part( 'templates/visitor', 'counter' ); ?></p>
-    <p><?php _e( 'Copyright &copy;', 'ukmtheme' ); ?><?php echo date( 'Y' ); ?><?php _e( 'The National University of Malaysia', 'ukmtheme' ); ?></p>
+    <p><?php _e( 'Copyright &copy;', 'ukmtheme' ); ?><?php echo date( 'Y' ); ?>&nbsp;<?php _e( 'The National University of Malaysia', 'ukmtheme' ); ?></p>
     <p><?php _e( 'Best view with any modern web browser (Desktop &amp; Mobile)', 'ukmtheme' ); ?></p>
     <div class="ut_phone_access_wrapper">
       <i class="uk-icon-mobile-phone ut_phone_access"></i>
--- inc/theme-plugins.php
+++ inc/theme-plugins.php
@@ -1,49 +1,31 @@
 <?php
 /**
- * This file represents an example of the code that themes would use to register
- * the required plugins.
+ * @link http://www.ukm.my/template
  *
- * It is expected that theme authors would copy and paste this code into their
- * functions.php file, and amend to suit.
+ * @package WordPress
+ * @subpackage ukmtheme
+ * @since 4.0
  *
- * @package    TGM-Plugin-Activation
- * @subpackage Example
- * @version    2.3.6
- * @author     Thomas Griffin <thomas@thomasgriffinmedia.com>
- * @author     Gary Jones <gamajo@gamajo.com>
- * @copyright  Copyright (c) 2012, Thomas Griffin
- * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
- * @link       https://github.com/thomasgriffin/TGM-Plugin-Activation
+ * @author Jamaludin Rajalu
+ *
+ * Required Plugins
  */
-
-/**
- * Include the TGM_Plugin_Activation class.
- */
-//require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 require_once get_template_directory() . '/lib/plugins/plugins.php';
 
 add_action( 'tgmpa_register', 'ukmtheme_register_required_plugins' );
-/**
- * Register the required plugins for this theme.
- *
- * In this example, we register two plugins - one included with the TGMPA library
- * and one from the .org repo.
- *
- * The variable passed to tgmpa_register_plugins() should be an array of plugin
- * arrays.
- *
- * This function is hooked into tgmpa_init, which is fired within the
- * TGM_Plugin_Activation class constructor.
- */
+
 function ukmtheme_register_required_plugins() {
 
-  /**
-   * Array of plugin arrays. Required keys are name and slug.
-   * If the source is NOT from the .org repo, then source is also required.
-   */
   $plugins = array(
 
-    // This is an example of how to include a plugin from the WordPress Plugin Repository
+    // Plugin lists
+
+    array(
+      'name'      => 'ARYO Activity Log',
+      'slug'      => 'aryo-activity-log',
+      'required'  => false,
+    ),
+
     array(
       'name'      => 'Polylang',
       'slug'      => 'polylang',
@@ -93,16 +75,8 @@ function ukmtheme_register_required_plugins() {
     ),
   );
 
-  // Change this to your theme text domain, used for internationalising strings
   $theme_text_domain = 'ukmtheme';
 
-  /**
-   * Array of configuration settings. Amend each line as needed.
-   * If you want the default strings to be available under your own theme domain,
-   * leave the strings uncommented.
-   * Some of the strings are added into a sprintf, so see the comments at the
-   * end of each line for what each argument will be.
-   */
   $config = array(
     'domain'                              => $theme_text_domain,          // Text domain - likely want to be the same as your theme.
     'default_path'                        => '',                          // Default absolute path to pre-packaged plugins
