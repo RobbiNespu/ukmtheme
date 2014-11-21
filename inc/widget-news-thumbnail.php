4a239b3546a732da9760ee0f5ccb365d5ff8e7cf
user <jrajalu@gmail.com>
2014-11-21 22:33:51 +0800

    no message

--- inc/widget-news-thumbnail.php
+++ inc/widget-news-thumbnail.php
@@ -51,28 +51,21 @@ class Latest_News_Widget_Thumbnail extends WP_Widget {
      */
 
 
-    $newsCatArgs = array(
+    $news_args = array(
       'post_type'       => 'news',
-      'posts_per_page'  => $newscount,
-      'newscat'         => $newscat,
+      'posts_per_page'  => $instance['newscount'],
+      'newscat'         => $instance['newscat'],
       );
 
-    $newsloop = new WP_Query( $newsCatArgs );
+    $news_query = new WP_Query( $news_args );
 
-    if ( $newsloop->have_posts() ) : while ( $newsloop->have_posts() ) : $newsloop->the_post(); ?>
+    if ( $news_query->have_posts() ) : while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
 
       <div class="ut-news-list clearfix">
         <div class="col-1-5 ut-news-thumb">
-          <?php
-
-            if ( has_post_thumbnail() ) {
-              the_post_thumbnail();
-            }
-
-            else {
-              echo '<img src="' . get_template_directory_uri() . '/assets/images/public/thumbnail.png?ver=6.3" height="auto" width="auto"/>';
-            }
-
+          <?php 
+            if ( has_post_thumbnail() ) { the_post_thumbnail(); }
+            else { echo '<img src="' . get_template_directory_uri() . '/assets/images/public/thumbnail.png" height="auto" width="auto"/>'; }
           ?>
         </div>
           <div class="col-4-5 ut-news-content-widget">
@@ -96,39 +89,34 @@ class Latest_News_Widget_Thumbnail extends WP_Widget {
 }
 
   public function form( $instance ) {
-    if ( isset( $instance[ 'title'] ) ) {
-      $title = $instance[ 'title' ];
+
+    if ( isset( $instance['title'] ) ) {
+      $title = $instance['title'];
     }
     else {
       $title = __( 'New title', 'ukmtheme' );
     }
 
-    if ( isset( $instance[ 'newscat'] ) ) {
-      $newscat = $instance[ 'newscat' ];
-    }
-    else {
-      $newscat = 'research';
+    if ( isset( $instance['newscat'] ) ) {
+      $newscat = $instance['newscat'];
     }
 
     if ( isset( $instance[ 'newscount'] ) ) {
       $newscount = $instance[ 'newscount' ];
     }
-    else {
-      $newscount = '4';
-    }
     
     ?>
     <p class="tukm-widget-text">
       <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
     </p>
     <p class="tukm-widget-text">
       <label for="<?php echo $this->get_field_id( 'newscat' ); ?>"><?php _e( 'News category slug:' ); ?></label> 
-      <input class="widefat" id="<?php echo $this->get_field_id( 'newscat' ); ?>" name="<?php echo $this->get_field_name( 'newscat' ); ?>" type="text" value="<?php echo esc_attr( $newscat ); ?>" />
+      <input class="widefat" id="<?php echo $this->get_field_id( 'newscat' ); ?>" name="<?php echo $this->get_field_name( 'newscat' ); ?>" type="text" value="<?php echo $newscat; ?>" />
     </p>
     <p class="tukm-widget-text">
       <label for="<?php echo $this->get_field_id( 'newscount' ); ?>"><?php _e( 'Number of news to show:','ukmtheme' ); ?></label> 
-      <input class="widefat" id="<?php echo $this->get_field_id( 'newscount' ); ?>" name="<?php echo $this->get_field_name( 'newscount' ); ?>" type="text" value="<?php echo esc_attr( $newscount ); ?>" />
+      <input class="widefat" id="<?php echo $this->get_field_id( 'newscount' ); ?>" name="<?php echo $this->get_field_name( 'newscount' ); ?>" type="text" value="<?php echo $newscount; ?>" />
     </p>
     <?php 
   }
@@ -145,7 +133,9 @@ class Latest_News_Widget_Thumbnail extends WP_Widget {
 }
 
 function register_Latest_News_Widget_Thumbnail() {
-    register_widget( 'Latest_News_Widget_Thumbnail' );
+  register_widget( 'Latest_News_Widget_Thumbnail' );
 }
+
 add_action( 'widgets_init', 'register_Latest_News_Widget_Thumbnail' );
+
 ?>
