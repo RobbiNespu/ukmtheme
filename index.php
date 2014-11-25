eb7ced961cc5fea58d14057a0e30436f68233cde
user <jrajalu@gmail.com>
2014-11-25 23:05:29 +0800

    no message

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ get_header(); ?>
 <article class="wrap">
 <div class="content clearfix">
 <section class="col-3-4 article">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <h2 class="content-title"><?php the_title(); ?></h2>
-      <div class="content-article">
-        <?php the_content(); ?>
-      </div>   
-      <?php endwhile; else: ?>
-        <h2><?php _e( 'Not Found', 'ukmtheme' ); ?></h2>
-        <p><?php _e( 'Sorry, no page matched your criteria.', 'ukmtheme' ); ?></p>
-    <?php endif; ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h2 class="content-title"><?php the_title(); ?></h2>
+    <div class="content-article">
+      <?php the_content(); ?>
+    </div>   
+    <?php endwhile; else: ?>
+      <h2><?php _e( 'Not Found', 'ukmtheme' ); ?></h2>
+      <p><?php _e( 'Sorry, no page matched your criteria.', 'ukmtheme' ); ?></p>
+  <?php endif; ?>
 </section>
 <aside class="col-1-4">
 	<?php if (dynamic_sidebar( 'sidebar-2' )) : else : ?><?php endif; ?>
