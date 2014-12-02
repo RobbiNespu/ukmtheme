b4d336d7e318854cf13d9ae17ac1a0a258511f0b
jrajalu <jrajalu@gmail.com>
2014-12-02 11:00:23 +0800

    no message

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@
   <p class="ut_copyright_statement"><?php _e( 'Copyright &copy;&nbsp;', 'ukmtheme' ); ?><?php echo date( 'Y' ); ?>&nbsp;<?php _e( 'The National University of Malaysia', 'ukmtheme' ); ?></p>
   <p class="ut_bestview_statement"><?php _e( 'This site can be accessed using smart devices. Best viewed using the latest versions of web browsers on a minimum resolution of 1024x768.<br/>For further explanation can be read <a href="http://browsehappy.com/" target="_blank">here</a>.', 'ukmtheme' ); ?></p>
   <p class="tukm-visitor-counter"><?php get_template_part( 'templates/visitor', 'counter' ); ?></p>
-  <p class="tukm-theme-version"><a href="http://www.ukm.my/template/" title="" target="_blank"><?php echo "v" . $ukmtheme->get( 'Version' ); ?></a></p>
+  <p class="tukm-theme-version"><a href="http://www.ukm.my/template/" title="" target="_blank"><?php echo "v" . wp_get_theme()->get( 'Version' ); ?></a></p>
 </div>
 </footer>
 <?php wp_footer(); ?>
