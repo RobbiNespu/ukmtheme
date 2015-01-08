bacf978dd3c3ff42ad7edb284e6e36b4d24b2dd1
user <jrajalu@gmail.com>
2015-01-08 23:07:35 +0800

    no message

--- templates/nav-search.php
+++ templates/nav-search.php
@@ -9,9 +9,11 @@
  * @author Jamaludin Rajalu
  */
  ?>
-<div class="uk-navbar-flip">
-<form class="uk-search uk-form-small" action="<?php echo home_url( '/' ); ?>">
-	<input class="uk-search-field" type="search" placeholder="<?php echo __( 'search...', 'ukmtheme' ); ?>" value="" name="s">
-	<button class="uk-search-close" type="reset"></button>
+<div class="uk-navbar-flip tukm-top-search-box">
+<form class="uk-form" action="<?php echo home_url( '/' ); ?>">
+<div class="uk-form-icon">
+<i class="uk-icon-search"></i>
+	<input class="uk-form-small" type="search" placeholder="<?php echo __( 'search...', 'ukmtheme' ); ?>" value="" name="s">
+</div>
 </form>
 </div>
