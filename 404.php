74c987dcf337c49e127701d4d1ed799822f43d08
user <user@users-Mac-Pro.local>
2015-01-20 12:22:05 +0800

    no message

--- 404.php
+++ 404.php
@@ -11,8 +11,8 @@
  */
 get_header(); ?>
 <article class="wrap">
-  <div class="content clearfix">
-    <section class="col-1-1 article">
+  <div class="pure-g content clearfix">
+    <section class="pure-u-1 article">
       <h2 class="content-title"><?php _e( 'Not Found', 'ukmtheme' ); ?></h2>
       <p><?php _e( 'Sorry, no page matched your criteria.', 'ukmtheme' ); ?></p>
       <?php get_search_form(); ?>
