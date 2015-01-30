da9baaef3f7eeb905c1b6a3dbb11a90e94b22900
jrajalu <jrajalu@gmail.com>
2015-01-30 09:15:55 +0800

    no message

--- functions.php
+++ functions.php
@@ -384,5 +384,4 @@ remove_filter( 'pre_term_description', 'wp_filter_kses' );
 remove_filter( 'pre_link_description', 'wp_filter_kses' );
 remove_filter( 'pre_link_notes', 'wp_filter_kses' );
 remove_filter( 'term_description', 'wp_kses_data' );
-
-/** end here */
+/** end */
