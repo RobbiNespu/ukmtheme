779d7ec7053d7ab1556a7606c49124234c449bfd
jrajalu <jrajalu@gmail.com>
2015-02-05 12:59:53 +0800

    no message

--- templates/taxonomy-pubcat.php
+++ templates/taxonomy-pubcat.php
@@ -17,7 +17,7 @@ get_header(); ?>
     <section class="pure-u-3-4 article">
     <h2 class="content-title"><?php single_cat_title(); ?></h2>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="pure-u-1-1 ut-publication-archive-wrap">
+        <div class="pure-g ut-publication-archive-wrap">
           <div class="pure-u-3-10 article ut-publication">
           <?php 
             $saved_data = get_post_meta($post->ID,'ut_publication_cover',true);
